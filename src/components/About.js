23ccf36dddb3faaaa8f6316e178c05c6b3f8a210
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2021-09-02 19:40:55 +0530

    V19 Fixed Issues

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,24 @@
 import React,{useState} from 'react'
 
-export default function About() {
+export default function About(props) {
 
-    const [myStyle,setMyStyle]=useState({
+   /* const [myStyle,setMyStyle]=useState({
         color:'black',
         backgroundColor:'white'
         
 
-    })
+    })*/
+
+    let myStyle ={
+      color : props.mode  ==='dark'?'white':'#042743',
+      backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white',
+     // border: '2px solid',
+     // borderColor: props.mode  ==='dark'?'white':'#042743'
+    }
 
     const[btnText,setBtnText]=useState("Enable Light Mode")
 
-    const toggleStyle = ()=>{
+  {/*  const toggleStyle = ()=>{
         if(myStyle.color ==='black'){
             setMyStyle({
                 color:'white',
@@ -32,7 +39,7 @@ export default function About() {
         }
 
 
-    }
+    }*/}
 
   /*  let myStyle={
         color:'white',
@@ -41,7 +48,7 @@ export default function About() {
 
     return (
 
-        <div className="container" style={myStyle}>
+        <div className="container" style={{color : props.mode  ==='dark'?'white':'#042743'}}>
                 <h1 className="my-3">About Us</h1>
                 <div className="accordion" id="accordionExample" style={myStyle}>
             <div className="accordion-item">
@@ -81,9 +88,9 @@ export default function About() {
             </div>
             </div>
         </div>
-        <div className="my-3">
+        {/*<div className="my-3">
         <button onClick={toggleStyle} type="button"  className="btn btn-primary">{btnText}</button>
-        </div>
+        </div>*/}
       
         </div>
     )
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -44,21 +44,21 @@ export default function Textform(props) {
     return (
         <>
     <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text}  onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text}  onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearText}>Clear Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopyText}>Copy Text</button>
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>0.008 * {text.split(" ").length} Minutes Read</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes Read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter Text to Preview Here"}</p>
+        <p>{text.length>0?text:"Nothing to Preview!"}</p>
     </div>
 
     </>
